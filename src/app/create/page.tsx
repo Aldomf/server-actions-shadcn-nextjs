beaa583d6e51dc08652b294933563a636865a916
Aldo Miralles <alditomiralles3@gmail.com>
2024-08-01 23:31:36 -0600

    first commit

---
+++ src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import { CardWithForm } from "@/components/Form";
+import React from "react";
+
+function Create() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <CardWithForm />
+    </div>
+  );
+}
+
+export default Create;
