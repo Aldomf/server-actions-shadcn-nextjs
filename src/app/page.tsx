f8a124258d82a61b06b9aa474a3ddea6ceab0093
Aldo Miralles <alditomiralles3@gmail.com>
2024-08-02 23:31:35 -0600

    creating create form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import prisma from "@/lib/prisma";
+import { Badge } from "@/components/ui/badge";
+import clsx from "clsx";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await prisma.task.findMany();
   return (
-   <div>hola</div>
+    <div className="grid grid-cols-3 gap-4 mt-10 px-10">
+      {tasks.map((task) => (
+        <Card key={task.id}>
+          <CardHeader className="flex flex-row justify-between">
+            <CardTitle>{task.title}</CardTitle>
+            <Badge
+              className={clsx({
+                "bg-green-500": task.priority === "low",
+                "bg-yellow-500": task.priority === "medium",
+                "bg-red-500": task.priority === "high",
+                "bg-violet-500": task.priority === "urgent",
+              })}
+            >
+              {task.priority}
+            </Badge>
+          </CardHeader>
+          <CardContent>
+            <p>{task.description}</p>
+            <span className="text-slate-300">
+              {new Date(task.createdAt).toLocaleDateString()}
+            </span>
+          </CardContent>
+          <CardFooter className="flex justify-end gap-x-2">
+            <Button variant={"destructive"}>Delete</Button>
+            <Button>Edit</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,52 +8,88 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export function CardWithForm() {
+  async function createTask(formData: FormData) {
+    "use server";
+
+    const rawFormData = {
+      title: formData.get("title") as string | null,
+      description: formData.get("description") as string | null,
+      priority: formData.get("priority") as string | null,
+    };
+
+    if (!rawFormData.title || !rawFormData.priority) {
+      throw new Error("Title and priority are required.");
+    }
+
+    const newTask = await prisma.task.create({
+      data: {
+        title: rawFormData.title,
+        description: rawFormData.description ?? "",
+        priority: rawFormData.priority,
+      },
+    });
+
+    console.log(newTask);
+    redirect("/");
+  }
+
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
+    <form action={createTask}>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Create task</CardTitle>
+          <CardDescription>Create your new task in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="title">Title</Label>
+              <Input id="title" placeholder="Title" name="title" />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                id="description"
+                placeholder="Description"
+                name="description"
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="priority">Priority</Label>
+              <Select name="priority">
+                <SelectTrigger id="priority">
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
                 <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                  <SelectItem value="low">Low</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="high">High</SelectItem>
+                  <SelectItem value="urgent">Urgent</SelectItem>
                 </SelectContent>
               </Select>
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
-    </Card>
-  )
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Create</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
